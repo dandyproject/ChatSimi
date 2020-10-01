99478db95a62eb80f74f65f4aa371cb7f043f028
Ahmad Dandi Subhani <63481398+dandyproject@users.noreply.github.com>
2020-10-01 12:11:38 +0700

    Update curl.php

--- curl.php
+++ curl.php
@@ -4,7 +4,7 @@
 
         // set url 
         $text = (isset($_GET["text"]) ? $_GET["text"] : "halo");
-        curl_setopt($ch, CURLOPT_URL, "http://sandbox.api.simsimi.com/request.p?key=API_KEY&lc=id&ft=0.5&text=" . urlencode($text)); 
+        curl_setopt($ch, CURLOPT_URL, "http://sandbox.api.simsimi.com/request.p?key=tHaPiOXzLFtBw2MmnQgdrA29jwoA_Cw3iItD-msS&lc=id&ft=0.5&text=" . urlencode($text)); 
 
         //return the transfer as a string 
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); 
